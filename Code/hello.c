139996a9f73d42231935335fd78e818235aaf7c3
Shreyansh <iit2016068@iiita.ac.in>
2020-01-16 01:06:24 -0800

    Implemented Feature1-Branching

--- Code/hello.c
+++ Code/hello.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
+void doBranching();
 int main()
 {
 	int a=10;
 	printf("I am don.");
 	printf("In hello.c");
+	doBranching();
 }
 void doSomething()
 {
 	
+}
+void doBranching()
+{
+	printf("Learning Branching!");
 }
